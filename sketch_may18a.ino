e99a029af4a7c3622d9f8aa3094deb4616b2c14b
Sabarivasan <144119741+Sabari-Vasan-SM@users.noreply.github.com>
2025-05-18 13:38:15 +0530

    Update sketch_may18a.ino

--- sketch_may18a.ino
+++ sketch_may18a.ino
@@ -1,4 +1,5 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 #include <SinricPro.h>
 #include <SinricProSwitch.h>
 
@@ -16,12 +17,18 @@
 #define RELAY1_PIN    5   // D1 = GPIO5
 #define RELAY2_PIN    4   // D2 = GPIO4
 
-// Callback function to handle power state changes
+// Web server on port 80
+ESP8266WebServer server(80);
+
+// Device IP as constant
+const char* deviceIP = "http://192.168.1.100";
+
+// Callback for SinricPro
 bool onPowerState(const String &deviceId, bool &state) {
   Serial.printf("Device %s turned %s\r\n", deviceId.c_str(), state ? "ON" : "OFF");
 
   if (deviceId == DEVICE_ID_1) {
-    digitalWrite(RELAY1_PIN, state ? LOW : HIGH);  // LOW = ON (Active Low)
+    digitalWrite(RELAY1_PIN, state ? LOW : HIGH);
   } else if (deviceId == DEVICE_ID_2) {
     digitalWrite(RELAY2_PIN, state ? LOW : HIGH);
   }
@@ -38,6 +45,8 @@ void setupWiFi() {
     Serial.print(".");
   }
   Serial.println(" Connected!");
+  Serial.print("Device IP Address: ");
+  Serial.println(WiFi.localIP());
 }
 
 // Sinric Pro Setup
@@ -52,17 +61,44 @@ void setupSinricPro() {
   SinricPro.restoreDeviceStates(true);
 }
 
-// Main Setup
+// Handle web request for relay1
+void handleRelay1Toggle() {
+  digitalWrite(RELAY1_PIN, !digitalRead(RELAY1_PIN));
+  server.send(200, "text/plain", String("Relay 1 is ") + (digitalRead(RELAY1_PIN) == LOW ? "ON" : "OFF"));
+}
+
+// Handle web request for relay2
+void handleRelay2Toggle() {
+  digitalWrite(RELAY2_PIN, !digitalRead(RELAY2_PIN));
+  server.send(200, "text/plain", String("Relay 2 is ") + (digitalRead(RELAY2_PIN) == LOW ? "ON" : "OFF"));
+}
+
+// Root page
+void handleRoot() {
+  String html = "<html><body><h1>ESP8266 Relay Control</h1>";
+  html += "<a href=\"/relay1\">Toggle Relay 1</a><br>";
+  html += "<a href=\"/relay2\">Toggle Relay 2</a><br>";
+  html += "</body></html>";
+  server.send(200, "text/html", html);
+}
+
+// Setup
 void setup() {
   Serial.begin(115200);
   pinMode(RELAY1_PIN, OUTPUT);
   pinMode(RELAY2_PIN, OUTPUT);
-
   digitalWrite(RELAY1_PIN, HIGH); // OFF
   digitalWrite(RELAY2_PIN, HIGH); // OFF
 
   setupWiFi();
   setupSinricPro();
+
+  // Setup web routes
+  server.on("/", handleRoot);
+  server.on("/relay1", handleRelay1Toggle);
+  server.on("/relay2", handleRelay2Toggle);
+  server.begin();
+  Serial.println("Web server started.");
 }
 
 // Loop
@@ -71,4 +107,5 @@ void loop() {
     setupWiFi();
   }
   SinricPro.handle();
+  server.handleClient();  // Handle web requests
 }
